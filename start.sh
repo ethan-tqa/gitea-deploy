1985e625dbd43aa83d1e099b3ecd10f44f554972
Ethan <>
2023-10-06 11:14:07 +0700

    change some backup paths

--- start.sh
+++ start.sh
@@ -4,7 +4,12 @@ mkdir -p gitea
 mkdir -p gitea/postgres
 mkdir -p gitea/data
 mkdir -p gitea/config
+mkdir -p backups
+mkdir -p backups/gitea-data
+mkdir -p backups/gitea-database
+mkdir -p backups/temp-postgres
 
 chmod -R 777 gitea
+chmod -R 777 backups
 
 docker compose up -d
